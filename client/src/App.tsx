c7a462fd81274744fdfb5d55737603906e094cf8
timmeno1 <stimka91@gmail.com>
2021-05-27 07:16:51 +0900

    tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-       Some App    
+    <div className="container mx-auto">
+      <Navbar />  
+       <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 text-4xl font-bold">Some App</h1>    
     </div>
   );
 }
---
+++ client/src/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export const Navbar = () => {
+
+
+    return (
+        <nav className="container px-4" >
+            <button>Home</button>
+            <button>News</button>
+            <button>Join</button>
+            <button>About</button>
+        </nav>
+    )
+}
