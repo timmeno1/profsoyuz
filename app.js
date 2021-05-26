00b1a0a5c6e9ae0e8b71d9d639b6b7802efc05e4
timmeno1 <stimka91@gmail.com>
2021-05-27 06:28:13 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+
+app.listen(port, () => {
+    console.log(`App started at port ${port}`)
+})
