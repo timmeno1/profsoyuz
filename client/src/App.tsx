d104e46a64f8871c5fce665c7e208b303658c689
timmeno1 <stimka91@gmail.com>
2021-05-29 21:00:12 +0900

    Revert "material ui"
    This reverts commit 4b4b4e70231be9421fac899a081c4f663391aa29.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,13 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import './App.css';
 import { Navbar } from './Navbar/Navbar';
 
-
-const outerTheme = createMuiTheme({
-  palette: {
-    primary: red,
-    secondary: {
-      main: '#b2dfdb',
-    },
-  },
-});
-
-
 function App() {
   return (
-    <ThemeProvider theme={outerTheme}>
-      <div className="container mx-auto">
-        <Navbar />  
-        <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 text-4xl font-bold">Some App</h1>    
-      </div>
-    </ThemeProvider>
+    <div className="container mx-auto">
+      <Navbar />  
+       <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 text-4xl font-bold">Some App</h1>    
+    </div>
   );
 }
 
--- client/src/Navbar/Navbar.tsx
+++ client/src/Navbar/Navbar.tsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import {Button, ButtonGroup} from '@material-ui/core'
 
 
 export const Navbar = () => {
 
 
     return (
-        <nav className="container flex justify-end py-4" >
-            <div className="logo">
-
-            </div>
-            <ButtonGroup className="buttons" variant="text" color="primary" aria-label="text primary button group">
-                <Button >Home</Button>
-                <Button >News</Button>
-                <Button >Join</Button>
-                <Button >About</Button>
-            </ButtonGroup>
+        <nav className="container px-4" >
+            <button>Home</button>
+            <button>News</button>
+            <button>Join</button>
+            <button>About</button>
         </nav>
     )
 }
