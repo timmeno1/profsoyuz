b578c8ccc34c1bf74e380e605b7ab2648b24d82c
timmeno1 <stimka91@gmail.com>
2021-05-29 21:01:06 +0900

    Revert "tailwindcss"
    This reverts commit c7a462fd81274744fdfb5d55737603906e094cf8.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './Navbar/Navbar';
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Navbar />  
-       <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 text-4xl font-bold">Some App</h1>    
+    <div className="App">
+       Some App    
     </div>
   );
 }
