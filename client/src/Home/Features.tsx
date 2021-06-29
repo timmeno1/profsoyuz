73810d82919b270c0a1b6f0051311882e06e795b
timmeno1 <stimka91@gmail.com>
2021-06-30 07:12:14 +0900

    Props to features, home props Object

--- client/src/Home/Features.tsx
+++ client/src/Home/Features.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
-export const Features = () => {
+type FeaturesType = {
+    icon: string
+    textHeader: string
+    text: string
+}
+type FeaturesPropsType = {
+    features : Array<FeaturesType>
+}
+
+export const Features = (props:FeaturesPropsType) => {
     return (
         <div className="section">
 
         <div className="row">
             <div className="col s12 m4">
             <div className="icon-block">
-                <h2 className="center brown-text"><i className="bi bi-app-indicator"></i></h2>
+                <h2 className="center brown-text"><i className={`bi ${props.features[0].icon}`}></i></h2>
                 <h5 className="center">Speeds up development</h5>
 
                 <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
@@ -16,7 +25,7 @@ export const Features = () => {
 
             <div className="col s12 m4">
             <div className="icon-block">
-                <h2 className="center brown-text"><i className="bi bi-bank2"></i></h2>
+                <h2 className="center brown-text"><i className={`bi ${props.features[1].icon}`}></i></h2>
                 <h5 className="center">User Experience Focused</h5>
 
                 <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
@@ -25,7 +34,7 @@ export const Features = () => {
 
             <div className="col s12 m4">
             <div className="icon-block">
-                <h2 className="center brown-text"><i className="bi bi-bookmark-star-fill"></i></h2>
+                <h2 className="center brown-text"><i className={`bi ${props.features[2].icon}`}></i></h2>
                 <h5 className="center">Easy to work with</h5>
 
                 <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
--- client/src/Home/Home.tsx
+++ client/src/Home/Home.tsx
@@ -4,16 +4,50 @@ import { Features } from './Features'
 import { Media } from './Media'
 import { PageHero } from './PageHero'
 
+
+let obj = {
+    PageHero: {
+        heroText: "Профсоюз",
+        heroImg: ""
+    },
+    Features: [
+        {
+            icon: "bi-calendar3",
+            textHeader: "textHeader",
+            text: "tralalala"
+        },
+        {
+            icon: "bi-shield-check",
+            textHeader: "textHeader",
+            text: "tralalala"
+        },
+        {
+            icon: "bi-watch",
+            textHeader: "textHeader",
+            text: "tralalala"
+        }
+    ],
+    Counters: {
+        members: 357,
+        solvedProblems: 539
+    },
+    Media: {
+        photos: [],
+        video: ""
+    }
+}
+
 export const Home = () => {
     
     console.log("have called")
 
     //M.toast({html: 'I am a toast!'})
 
+
     return (
         <div>
             <PageHero />
-            <Features />
+            <Features features={obj.Features}/>
             <Counters />
             <Media />
         </div>
