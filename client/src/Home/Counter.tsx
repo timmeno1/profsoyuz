a3f589e98c4036083c52427df149645ac09eb366
timmeno1 <stimka91@gmail.com>
2021-07-25 21:51:48 +0900

    animated numbers added to front page

--- client/src/Home/Counter.tsx
+++ client/src/Home/Counter.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import AnimatedNumber from "react-animated-numbers"
 
 export const Counters = () => {
+
+
+
     return (
-        <div>
-            <h1>Counters</h1>
+        <div className={"countersBlock"} >
+            <div className={"membersCounter"} >
+                <span>Количество участников: </span>
+                <AnimatedNumber
+                    fontStyle={{ fontSize: 40 }}
+                    animateToNumber={376}
+                    config={{ tension: 89, friction: 40 }}
+                    animationType={"calm"}
+                />
+            </div>
+            <div className={"solvedCounter"} >
+                <span>Количество решенных проблем: </span>
+                <AnimatedNumber
+                    fontStyle={{ fontSize: 40 }}
+                    animateToNumber={531}
+                    config={{ tension: 89, friction: 40 }}
+                    animationType={"calm"}
+                />
+            </div>
         </div>
     )
 }
